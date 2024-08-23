d1fe3abd54a09efff5d0539fe998d9464e4bf6f6
kaleb110 <137324547+kaleb110@users.noreply.github.com>
2024-08-24 00:09:22 +0300

    type error

--- app/lib/sanityClient.tsx
+++ app/lib/sanityClient.tsx
@@ -1,9 +1,10 @@
+
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION, // Update to the latest API version
+  projectId: "cmirqaaf",
+  dataset: "production",
+  apiVersion: "2022-03-07", // Update to the latest API version
   useCdn: false, // Set to false to always fetch the latest data
   token: process.env.SANITY_API_TOKEN,
 });
