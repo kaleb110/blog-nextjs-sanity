47b53e95514b34a7028c43e04dc85df4b191b46b
kaleb110 <137324547+kaleb110@users.noreply.github.com>
2024-08-24 00:27:00 +0300

    api error

--- app/lib/sanityClient.tsx
+++ app/lib/sanityClient.tsx
@@ -1,11 +1,13 @@
+
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
+  projectId: "cmirqaaf",
+  dataset: "production",
   apiVersion: "2022-03-07", // Update to the latest API version
   useCdn: false, // Set to false to always fetch the latest data
-  token: process.env.SANITY_API_TOKEN,
+  token:
+    "skruyOxf3HhqMd6zIBGVMd1Xj1dCy7TnBBNKKSx1aRUj9pk12mVYf6kfpmvI07M7Z6pWnoyrQ4DXvkofsJL22cal4ENsAEhPOE532akuBNpzfUZ0UAJxI1zqYb0WzzaFWTvLSSDd6kiSoa1eGLMl5v7MergMDsOQrYZz02P81OK6Epzk3Iby",
 });
 
 export default client;
